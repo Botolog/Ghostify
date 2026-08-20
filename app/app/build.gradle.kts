61c793670217979af9bb104246de2dbcb68405cc
root <root@localhost.localdomain>
2026-08-20 11:50:41 +0000

    bump: versionCode 20→21, versionName 0.1.19→0.1.20

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "xyz.botolog.ghostify"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.1.19"
+        versionCode = 21
+        versionName = "0.1.20"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
