4ac67893d04c98460806adc8a1c9459b91fc16b1
Botolog <bot.botolog@gmail.com>
2026-09-12 04:11:21 +0300

    bump to 0.2.0 :)
    somewhat usable

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "xyz.botolog.ghostify"
         minSdk = 26
         targetSdk = 35
-        versionCode = 44
-        versionName = "0.1.43"
+        versionCode = 45
+        versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
